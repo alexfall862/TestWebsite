922d3f434c8ddbc15831970523082fc0405feafa
alexfall862 <alexfall862@gmail.com>
2025-06-16 00:37:14 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/TestWebsite/', // <-- Replace with your actual repo name
   plugins: [react()],
+  base: '/TestWebsite/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
