d9e1ab694864b69f55277dc42f69a964b5ed664c
alexfall862 <alexfall862@gmail.com>
2025-06-16 12:54:53 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/TestWebsite/',
-  build: {
-    outDir: 'dist',
-    target: 'es2015',
-    rollupOptions: {
-      output: {
-        format: 'iife', // Change from 'es' to 'iife'
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  }
 })
